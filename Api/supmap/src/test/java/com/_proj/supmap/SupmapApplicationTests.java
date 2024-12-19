588947ab1ff40c102cc3a22fd9fc0e03df87fde6
Abdoul_waris <abdoulwariskonate@gmail.com>
2024-12-19 13:51:48 +0100

    initialiation architecture api Spring boot

---
+++ Api/supmap/src/test/java/com/_proj/supmap/SupmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com._proj.supmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
