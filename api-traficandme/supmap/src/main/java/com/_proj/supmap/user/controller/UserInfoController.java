d4de8385b71014eaaf0b0525aabfa5876dab0804
the coder <abdoulwariskonate@gmail.com>
2025-02-13 22:09:50 +0100

    Initialisation des classes User

---
+++ api-traficandme/supmap/src/main/java/com/_proj/supmap/user/controller/UserInfoController.java
@@ -0,0 +1,9 @@
+package com._proj.supmap.user.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class UserInfoController {
+}
--- api-traficandme/supmap/src/main/java/com/_proj/supmap/user/model/UserInfo.java
+++ api-traficandme/supmap/src/main/java/com/_proj/supmap/user/model/UserInfo.java
@@ -1,3 +1,13 @@
-public class UserInfo {
+package com._proj.supmap.user.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserInfo {
+    int Id;
+    String Name;
+    String Email;
+    String PhoneNumber;
 }
