b6a285c63961db1a89f9241c30b335a0f71cc014
BELYAHIAOUI Hamza ADMIN <hamza.belyahiaoui@FRLT284411.ce.int.amecspie.com>
2025-02-14 22:45:41 +0100

    update front

--- web-traficandme/src/app/page/incentives/incentives.component.ts
+++ web-traficandme/src/app/page/incentives/incentives.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {TranslatePipe} from '@ngx-translate/core';
+import {AccordionModule} from 'primeng/accordion';
 
 @Component({
   selector: 'app-incentives',
-  imports: [],
+  imports: [
+    TranslatePipe,AccordionModule
+  ],
   templateUrl: './incentives.component.html',
   standalone: true,
   styleUrl: './incentives.component.css'
 })
 export class IncentivesComponent {
+
+  tabs = [
+    { title: 'Title 1', content: 'Content 1', value: '0' },
+  ];
 
 }
