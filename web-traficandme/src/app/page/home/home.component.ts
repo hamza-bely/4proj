f0a28a1a5958fe961665ba5eba832fa321eca9dd
BELYAHIAOUI Hamza ADMIN <hamza.belyahiaoui@FRLT284411.ce.int.amecspie.com>
2025-02-16 21:18:18 +0100

    fix

--- web-traficandme/src/app/page/home/home.component.ts
+++ web-traficandme/src/app/page/home/home.component.ts
@@ -3,10 +3,11 @@ import {TranslateModule} from '@ngx-translate/core';
 import {IncentivesComponent} from '../../components/incentives/incentives.component';
 import {StatsComponent} from '../../components/stats/stats.component';
 import {ContentSectionComponent} from '../../components/content-section/content-section.component';
+import {ContactComponent} from '../../components/contact/contact.component';
 
 @Component({
   selector: 'app-home',
-  imports: [TranslateModule, IncentivesComponent, StatsComponent, ContentSectionComponent],
+  imports: [TranslateModule, IncentivesComponent, StatsComponent, ContentSectionComponent,ContactComponent],
   templateUrl: './home.component.html',
   standalone: true,
   styleUrl: './home.component.css'
