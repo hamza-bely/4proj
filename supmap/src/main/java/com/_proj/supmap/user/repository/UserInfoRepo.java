eb8828655582984e8f9b9cf9d61142c5daf7de28
the coder <abdoulwariskonate@gmail.com>
2025-02-24 21:27:26 +0100

    suppression de SupMap

--- supmap/src/main/java/com/_proj/supmap/user/repository/UserInfoRepo.java
+++
@@ -1,9 +0,0 @@
-package com._proj.supmap.user.repository;
-
-import com._proj.supmap.user.model.UserInfo;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserInfoRepo extends CrudRepository<Integer, UserInfo> {
-}
--- supmap/src/main/java/com/_proj/supmap/user/service/UserInfoRepoService.java
+++
@@ -1,7 +0,0 @@
-package com._proj.supmap.user.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserInfoRepoService {
-}
