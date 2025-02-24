7d28f57f05daffaadb049179623fcbe7f21a6610
the coder <abdoulwariskonate@gmail.com>
2025-02-24 16:39:35 +0100

    decoupage du backend

--- api-traficandme/src/main/java/com/supinfo/api_traficandme/ApiTrafficAndMeApplication.java
+++ api-traficandme/src/main/java/com/supinfo/api_traficandme/ApiTrafficAndMeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiTraficandmeApplication {
+public class ApiTrafficAndMeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiTraficandmeApplication.class, args);
+		SpringApplication.run(ApiTrafficAndMeApplication.class, args);
 	}
 
 }
