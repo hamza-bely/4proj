16337015c6278f482273fca7ddb4c4a739829516
the coder <abdoulwariskonate@gmail.com>
2025-02-24 16:26:26 +0100

    archi du projet

---
+++ api-traficandme/src/test/java/com/supinfo/api_traficandme/ApiTraficandmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supinfo.api_traficandme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTraficandmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
