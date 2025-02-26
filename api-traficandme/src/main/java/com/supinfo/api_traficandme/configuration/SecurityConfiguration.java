923092be6a4205f35ac90415088487a01e073525
the coder <abdoulwariskonate@gmail.com>
2025-02-26 23:37:43 +0100

    ajout de l'authentification par des Providers Externes

--- api-traficandme/src/main/java/com/supinfo/api_traficandme/configuration/SecurityConfiguration.java
+++ api-traficandme/src/main/java/com/supinfo/api_traficandme/configuration/SecurityConfiguration.java
@@ -2,15 +2,32 @@ package com.supinfo.api_traficandme.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration {
+
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
-        return null;
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+
+       return httpSecurity
+               .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize ->
+                        authorize.requestMatchers("/").permitAll()
+                                .anyRequest().authenticated())
+               .oauth2Login(Customizer.withDefaults())
+               .formLogin(Customizer.withDefaults())
+               .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- api-traficandme/src/main/java/com/supinfo/api_traficandme/controller/UserInfoController.java
+++ api-traficandme/src/main/java/com/supinfo/api_traficandme/controller/UserInfoController.java
@@ -1,4 +1,19 @@
 package com.supinfo.api_traficandme.controller;
 
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/user")
 public class UserInfoController {
+
+    @GetMapping("/info")
+    public Map<String, Object> userInfo(OAuth2AuthenticationToken authentication) {
+        // Return the user's attributes as a map
+        return authentication.getPrincipal().getAttributes();
+    }
 }
