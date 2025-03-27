dbd8ffbd40cc52794aa5dd2ebdf844d7777f07bc
AbdoulWaris <kwaris@did-itech.com>
2025-03-27 16:07:42 +0100

    hamza

--- api-traficandme/src/main/java/com/supinfo/api_traficandme/User/repository/UserRepository.java
+++ api-traficandme/src/main/java/com/supinfo/api_traficandme/User/repository/UserRepository.java
@@ -1,12 +1,13 @@
 package com.supinfo.api_traficandme.User.repository;
 
 import com.supinfo.api_traficandme.User.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository  extends CrudRepository<UserInfo, Integer> {
+public interface UserRepository  extends JpaRepository<UserInfo, Integer> {
     Optional<UserInfo> findByEmail(String email);
 }
