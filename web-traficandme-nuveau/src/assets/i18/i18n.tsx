b9c69c10d5cd2ca6f35392fc8ef6a6b0456ed969
hamza-bely <hamzaciao80@gmail.com>
2025-04-30 22:34:26 +0200

    fix front

--- web-traficandme-nuveau/src/assets/i18/i18n.tsx
+++ web-traficandme-nuveau/src/assets/i18/i18n.tsx
@@ -12,8 +12,8 @@ i18n
     .use(initReactI18next)
     .init({
         resources,
-        lng: localStorage.getItem("language") || "en",
-        fallbackLng: "en",
+        lng: localStorage.getItem("language") || "fr",
+        fallbackLng: "fr",
         interpolation: { escapeValue: false }
     });
 
